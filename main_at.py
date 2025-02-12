955440a47111018a6e6f49db0ebcb473c3461253
cgobb <68285450+cgobb@users.noreply.github.com>
2025-02-12 09:44:28 -0500

    Add files via upload

---
+++ main_at.py
@@ -0,0 +1,64 @@
+import csv
+import io
+import shutil
+
+# https://docs.python.org/3/library/csv.html#csv.Dialect
+# excel-tab
+
+# My Project tutu@cogeco.com
+
+def open_tsv(filename):
+  
+  f_ = io.StringIO()
+  
+  with open(filename, "r") as f:
+    
+    filecopy = shutil.copyfileobj(f, f_)
+    
+    # Return to position 0 after the copy
+    f_.seek(0)
+    
+    # TSV Reader to Dict object
+    google_sheets_tsv_export = csv.excel_tab()
+    reader = csv.DictReader(f_, dialect=google_sheets_tsv_export)
+  
+    return reader
+    
+
+if __name__ == "__main__":
+  
+  # import sys
+  #
+  # filename = sys.argv[1]
+  #
+  # t = open_tsv(filename)
+  #
+  # rows = [r for r in t]
+  #
+  # print("file:\t{}\nfields:\t{}".format(filename, t.fieldnames))
+  
+  import glob
+    
+    
+  # Fields from the 1st row
+  for f in glob.glob("./*.tsv"):
+    
+    t = open_tsv(f)
+    print("file:\t{}\nfields:\t{}\n  ".format(f, t.fieldnames))
+    
+  
+  # Samples
+  import pprint
+  for f in glob.glob("./*.tsv"):
+    
+    t = open_tsv(f)
+
+    rows = [r for r in t]
+
+    print("### SAMPLE {}\n\n".format(f))
+
+    for r in range(0, 2):
+      if r < len(rows):
+        pprint.pprint(rows[r])
+    
+    print("###\n\n")
